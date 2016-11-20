f9e357897f394f4079131a5b28486490c9bb462e
AndriiNikitin <AndriiNikitin@users.noreply.github.com>
2016-11-20 14:00:01 +0100

    Update fix-policy-rc.d.sh

--- fix-policy-rc.d.sh
+++ fix-policy-rc.d.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # use this hack to allow mysqld service start on ubuntu docker image
 # can be used e.g. like this:
-# docker run -v $(pwd):/test --rm --name mm ubuntu:16.04 bash -c "cd test; ./fix-policy-rc.d.sh ; ./ubuntu_install_mariadb_from_mirror.sh.sh"
+# docker run -v $(pwd):/test --rm --name mm ubuntu:16.04 bash -c "cd test; ./fix-policy-rc.d.sh ; ./ubuntu_install_mariadb_from_mirror.sh"
 
 if [ "`wc -l < /usr/sbin/policy-rc.d`" == "2" ] && [ "`tail -n 1 /usr/sbin/policy-rc.d`" == "exit 101" ] 
 then
